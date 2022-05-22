005f765e3e596b3e8a89bf85b69be28051f80c02
C. Masloch <pushbx@ulukai.org>
2022-05-22 22:22:05 +0200

    try to set test.sh executable

--- ci_test.sh
+++ ci_test.sh
@@ -16,6 +16,9 @@ echo GCC and Watcom kernels have all been built
 find _output -ls
 
 cd test
+ls -l
+chmod a+x test.sh
+ls -l
 if ! ./test.sh ../_output/gcc/KGC${KVER}.sys diskgc bootgc 'boot gcc: '
 then
   echo GCC boot test failed
